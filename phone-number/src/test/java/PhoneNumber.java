6ffce7ae6f7da14222fc09f133f03629b9daf0de
Lennart K <lennart.k@thinking-aloud.eu>
2015-09-04 21:24:19 +0200

    Exercise 6

---
+++ phone-number/src/test/java/PhoneNumber.java
@@ -0,0 +1,18 @@
+public class PhoneNumber {
+    private String number, areaCode;
+
+    public PhoneNumber(String s) {
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public String pretty() {
+        return null;
+    }
+}
